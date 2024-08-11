09051867d1ab9c5f45d03a462c86e23bddced443
unknown <puneethkumarvh@gmail.com>
2024-08-11 23:20:07 +0530

    Initialise the project

---
+++ src/test/java/com/puneeth/QuizeApp/QuizeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puneeth.QuizeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
